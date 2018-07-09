1b37faea729e260cdf7e6a0e476534df6a942778
Val <dgm97@cornell.edu>
2018-07-09 11:53:27 -0700

    basic truffle testing template

---
+++ test/TestQueery.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Queery.sol";
+
+contract TestQueery {
+  Queery queery = Queery(DeployedAddresses.Queery());
+
+  function testSameABIPackedStrings() public {
+    
+  }
+
+  function testIsBetter() public {
+
+  }
+
+  function testPersonExists() public {
+
+  }
+
+  function testRegisterPerson() public {
+
+  }
+
+  function testCalculateMultiplier() public {
+
+  }
+
+  function testPayout() public {
+
+  }
+
+  function testBet() public {
+
+  }
+
+
+}
